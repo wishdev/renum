371bffa54ebfe8cc568c03e3e6b89e7ae64af850
John W Higgins <wishdev@gmail.com>
2010-08-13 21:54:10 -0700

    Person updates to make it work nicer for me

--- lib/renum/enumerated_value.rb
+++ lib/renum/enumerated_value.rb
@@ -2,46 +2,64 @@ require 'forwardable'
 
 module Renum
 
-  # This is the superclass of all enumeration classes. 
-  # An enumeration class is Enumerable over its values and also delegates [] 
+  # This is the superclass of all enumeration classes.
+  # An enumeration class is Enumerable over its values and also delegates []
   # to the values array.
   # Values are also comparable, sorting into the order in which they're declared.
   class EnumeratedValue
-    
+
     class << self
       include Enumerable
       extend Forwardable
-      
-      def_delegators :values, :each, :[]
-      
+
+      def_delegators :values, :each
+
       # Returns an array of values in the order they're declared.
       def values
         @values ||= []
       end
-      
+
+      def find(name)
+        detect{|v| v.name == name}
+      end
+
+      def [](val)
+        case val
+          when Integer
+            @values[val]
+          when EnumeratedValue
+            @values[val.to_i]
+          when String
+            find(val)
+        end
+      end
     end
-    
+
     include Comparable
     attr_reader :name
     attr_reader :index
-    
+
     def initialize name
       @name = name.to_s
       @index = self.class.values.size
       self.class.values << self
     end
-    
+
     # Returns the fully qualified constant referring to this value.
-    # Don't override this if you're using Renum with the constantize_attribute 
+    # Don't override this if you're using Renum with the constantize_attribute
     # plugin, which relies on this behavior.
     def to_s
       "#{self.class}::#{name}"
     end
-    
+
+    def to_i
+      index
+    end
+
     # Sorts enumerated values into the order in which they're declared.
     def <=> other
       index <=> other.index
     end
-    
+
   end
 end
